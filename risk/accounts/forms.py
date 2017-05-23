a9430c48b2568717353c3d488d20a4b0e5c12cb1
ShivamBh <shivam.bhattacharjee94@gmail.com>
2017-05-23 22:39:25 +0530

    Profile model added

--- risk/accounts/forms.py
+++ risk/accounts/forms.py
@@ -1,5 +1,24 @@
 from django import forms 
+from django.contrib.auth.models import User
+from .models import Profile
+
+class UserRegistrationForm(forms.ModelForm):
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+	location = forms.CharField(label='Location of interest')
+
+	class Meta:
+		model = Profile
+		fields = ('username', 'first_name', 'last_name', 'email')
+
+	def clean_password2(self):
+		cd = self.cleaned_data
+		if cd['password'] != cd['password2']:
+			raise forms.ValidationError('passwords don\'t match.')
+		return cd['password2']
+
 
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput)
+
--- risk/accounts/models.py
+++ risk/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	username = models.CharField(max_length=120)
+	first_name = models.CharField(max_length=120)
+	last_name = models.CharField(max_length=150)
+	email = models.EmailField()
+	location = models.CharField(max_length=150, blank=False, null=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
